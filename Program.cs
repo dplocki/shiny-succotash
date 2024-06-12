d2a31eea899b8044c5705f500b8df466f9d26056
Dawid Płocki <dawid.plocki@gmail.com>
2024-06-12 21:30:57 +0200

    feat: add example from tutorial

--- Program.cs
+++ Program.cs
@@ -1,6 +1,22 @@
+using Microsoft.AspNetCore.OData;
+using Microsoft.OData.ModelBuilder;
+using ShinySuccotash.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var modelBuilder = new ODataConventionModelBuilder();
+modelBuilder.EntityType<Order>();
+modelBuilder.EntitySet<Customer>("Customers");
+
+builder.Services.AddControllers().AddOData(
+    options => options.Select().Filter().OrderBy().Expand().Count().SetMaxTop(null).AddRouteComponents(
+        "odata",
+        modelBuilder.GetEdmModel()));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseRouting();
+
+app.UseEndpoints(endpoints => endpoints.MapControllers());
 
 app.Run();
